94451f077bfaad5f3307fb78f46d8db47070b0da
Justin R. St-Amant <jstamant24@gmail.com>
2024-01-26 07:34:00 -0600

    Added hover effect to social links in footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
@@ -11,10 +9,10 @@ const Footer = () => {
         <div className="my-8">
           <h1 className="uppercase font-bold mb-4 text-xl">Socials</h1>
           <nav className="text-2xl">
-            <a className="px-1" href="https://github.com/jstamant"><FontAwesomeIcon icon={faGithub} /></a>
-            <a className="px-1" href="https://www.linkedin.com/in/justinstamant/"><FontAwesomeIcon icon={faInstagram} /></a>
-            <a className="px-1" href="https://www.instagram.com/jstamant_/"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a className="px-1" href="mailto:jstamant24@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+            <a className="px-1 duration-300 hover:text-neutral-400" href="https://github.com/jstamant"><FontAwesomeIcon icon={faGithub} /></a>
+            <a className="px-1 duration-300 hover:text-neutral-400" href="https://www.linkedin.com/in/justinstamant/"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a className="px-1 duration-300 hover:text-neutral-400" href="https://www.instagram.com/jstamant_/"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a className="px-1 duration-300 hover:text-neutral-400" href="mailto:jstamant24@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
           </nav>
         </div>
         <div className="my-8">
