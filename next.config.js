c3c6339362abe6b02050353f7c28320c62eef508
Justin St-Amant <jstamant24@gmail.com>
2024-07-28 21:35:22 -0500

    Updated resume, title, and skills

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   output: 'export',
   env: {
     site: {
-      subtitle: 'A Frontend Web Developer, ready to hit the ground running on your codebase.',
+      subtitle: 'A Full-Stack Web Developer, ready to hit the ground running on your codebase.',
     },
   },
 }
